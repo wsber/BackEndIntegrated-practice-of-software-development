62b1e6c3534645fa012b95b97b28d07218ace5d6
I-love-SUN <814172489@qq.com>
2023-01-14 14:06:40 +0800

    test查看pull request 是否能用

--- src/main/java/com/ws/springboot/utils/CodeGenerator.java
+++ src/main/java/com/ws/springboot/utils/CodeGenerator.java
@@ -13,23 +13,23 @@ public class CodeGenerator {
 
     }
     private static void generate(){
-        FastAutoGenerator.create("jdbc:mysql://localhost:3306/test?useSSL=false&useUnicode=true&characterEncoding=UTF-8", "root", "")
+        FastAutoGenerator.create("jdbc:mysql://localhost:3306/书吧管理系统-网页设计?useSSL=false&useUnicode=true&characterEncoding=UTF-8", "root", "longjianchao3618")
                 .globalConfig(builder -> {
                     builder.author("baomidou") // 设置作者
                             .enableSwagger() // 开启 swagger 模式
                             .fileOverride() // 覆盖已生成文件
-                            .outputDir("D:\\projects\\IntegratedPracticeOfSoftwareDevelopment\\Integrated-practice-of-software-development\\src\\main\\java\\"); // 指定输出目录
+                            .outputDir("C:\\Users\\long\\Documents\\软件系统综合实践\\Integrated-practice-of-software-development\\src\\main\\java\\"); // 指定输出目录
                 })
                 .packageConfig(builder -> {
                     builder.parent("com.ws.springboot") // 设置父包名
                             .moduleName("") // 设置父包模块名
-                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml, "D:\\projects\\IntegratedPracticeOfSoftwareDevelopment\\Integrated-practice-of-software-development\\src\\main\\resources\\mapper\\")); // 设置mapperXml生成路径
+                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml, "C:\\Users\\long\\Documents\\软件系统综合实践\\Integrated-practice-of-software-development\\src\\main\\resources\\mapper\\")); // 设置mapperXml生成路径
                 })
                 .strategyConfig(builder -> {
                     builder.entityBuilder().enableLombok();
                     builder.controllerBuilder().enableHyphenStyle()  // 开启驼峰转连字符
                             .enableRestStyle();  // 开启生成@RestController 控制器
-                    builder.addInclude("ruralrecreation") // 设置需要生成的表名
+                    builder.addInclude("location") // 设置需要生成的表名
                             .addTablePrefix("t_", "c_"); // 设置过滤表前缀
                 })
 //                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
